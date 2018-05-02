b1a0b0aef42e4ce8385ba22abf3b6138a463501d
windorz <1925449823@qq.com>
2018-05-02 09:53:03 +0800

    创建项目

---
+++ SimpleRequest/SimpleRequest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleRequest
+//
+//  Created by windorz on 2018/5/2.
+//  Copyright © 2018年 windorz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleRequest/SimpleRequest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleRequest
+//
+//  Created by windorz on 2018/5/2.
+//  Copyright © 2018年 windorz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
